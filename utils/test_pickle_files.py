dd1baa83165f06dfccd340a3c056f004e2db177e
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2024-03-24 01:24:37 -0700

    update pl_version

---
+++ utils/test_pickle_files.py
@@ -0,0 +1,40 @@
+import dataclasses
+import operator
+import time
+import pathlib
+import pickle
+import shortuuid
+import urllib3
+# import boto3
+import numpy as np
+import zstandard as zstd
+
+
+
+def decompress_datapoint(cbuf):
+  cctx = zstd.ZstdDecompressor()
+  buf = cctx.decompress(cbuf)
+  x = pickle.loads(buf)
+  return x
+
+
+
+data_path = '/data_2/ShAPO_Data/CAMERA/train'
+
+
+all_files = os.listdir(data_path)
+
+i=0
+for file in all_files:
+    
+    if i >10000:
+        print("Done with", i, "files")
+    try:
+        with open(path, 'rb') as fh:
+        dp = decompress_datapoint(fh.read())
+
+    except Exception as e:
+        print(e)
+        print("Error in file: ", file)
+        continue
+    i+=1
